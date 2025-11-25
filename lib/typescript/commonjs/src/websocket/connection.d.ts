494c0f5e6ebd539d68cb0f772c13bd247865a045
Sotatek-ElvisPhuong <elvis.phuong@sotatek.com>
2025-11-25 16:01:53 +0700

    feat: update lib

--- lib/typescript/commonjs/src/websocket/connection.d.ts
+++ lib/typescript/commonjs/src/websocket/connection.d.ts
@@ -1,19 +1,28 @@
 import { EventEmitter } from 'events';
+interface WSMessage {
+    method?: string;
+    [key: string]: any;
+}
 export declare class WebSocketClient extends EventEmitter {
     private ws;
     private url;
     private pingInterval;
+    private reconnectTimeout;
     private reconnectAttempts;
     private maxReconnectAttempts;
     private initialReconnectDelay;
     private maxReconnectDelay;
+    private messageQueue;
+    private isManuallyClosed;
     constructor(testnet?: boolean);
     getWebsocketState(): number | undefined;
     connect(): Promise<void>;
     private reconnect;
     private startPingInterval;
     private stopPingInterval;
-    sendMessage(message: any): void;
+    sendMessage(message: WSMessage): void;
+    private flushMessageQueue;
     close(): void;
 }
+export {};
 //# sourceMappingURL=connection.d.ts.map
